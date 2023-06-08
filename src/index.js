720ac072d845e4cb1ee8f841e7c2892cd73b9df2
Danica <danicas.edu@gmail.com>
2023-06-07 17:41:46 -0700

    changes temperature color

--- src/index.js
+++ src/index.js
@@ -17,3 +17,30 @@ const decreaseTempBtn = () => {
 };
 
 decreaseTempControl.addEventListener('click', decreaseTempBtn);
+
+const changeColorBasedOnTemp = () => {
+  const tempValue = document.getElementById('tempValue');
+  const temperature = tempValue.textContent;
+  
+
+
+  if(count >= 90) {
+    tempValue.style.color = 'red';
+  } else if(temperature >= 70 && temperature >= 79) {
+    tempValue.style.color = 'orange';
+  } else if(temperature >= 60 && temperature >= 69) {
+    tempValue.style.color = 'yellow';
+  } else if(temperature >= 50 && temperature >= 59) {
+    tempValue.style.color = 'green'
+  } else if(count <= 49) {
+    tempValue.style.color = 'teal';
+  }
+};
+
+// add the color classes here
+
+
+// changeColorBasedOnTemp();
+// document.getElementById('tempValue').style.color = "red";
+increaseTempControl.addEventListener('click', changeColorBasedOnTemp);
+decreaseTempControl.addEventListener('click', changeColorBasedOnTemp);
